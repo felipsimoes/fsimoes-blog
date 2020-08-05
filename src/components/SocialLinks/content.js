21569a5c9f8e17101a075d4c35523e9907d2c48f
Felipe Simoes <felipe_lima_simoes@hotmail.com>
2020-08-05 18:36:16 -0300

    adding styled-icons and rendering SocialLinks for Profile

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,18 @@
+const handle = "felipsimoes"
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`,
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/${handle}`,
+  },
+  {
+    label: "Linkedin",
+    url: `https://linkedin.com/${handle}`,
+  }
+]
+
+export default links
