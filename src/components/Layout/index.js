3288797b541228d37e3d42788baa59e1ff12e549
Felipe Simoes <felipe_lima_simoes@hotmail.com>
2020-08-05 18:01:16 -0300

    include styled components lib and plugin and add it to Layout and Avatar components

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,22 +1,19 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import Profile from "./Profile"
+import Profile from "../Profile"
+
+import GlobalStyles from '../../styles/global'
+import * as S from './styled';
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <S.LayoutWrapper>
+      <GlobalStyles />
       <aside>
         <Profile />
       </aside>
-      <main>{children}</main>
-    </>
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWrapper>
   )
 }
 
